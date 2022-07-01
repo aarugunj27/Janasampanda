3aeacfa8ca8ea9599ca7be230a7751650cb95896
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-07-01 10:58:02 -0500

    Update donate.js

--- website/src/pages/donate.js
+++ website/src/pages/donate.js
@@ -6,6 +6,48 @@ function Donate() {
         <div className="text">
             <h1>Donate</h1>
         </div>
+        <GooglePayButton
+        enviornment="TEST"
+        paymentRequest={{
+            apiVersion: 2,
+            apiVersionMinor: 0,
+            allowedPaymentMethods: [
+              {
+                type: 'CARD',
+                parameters: {
+                    allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                    allowedCardNetworks: ['MASTERCARD', 'VISA', 'AMERICAN EXPRESS'],
+                },
+                tokenizationSpecification: {
+                    type: 'PAYMENT_GATEWAY', 
+                    parameters: {
+                        gateway: 'example'
+                        gatewayMerchantId: 'exampleGatewayMerchantId',
+                    },
+                },
+            },
+        ],
+        merchantInfo: {
+            merchantId: '1234567890'
+            merchantName: 'Demo Merchant',
+        },
+        transactionInfo: {
+            totalPriceStatus: 'FINAL',
+            totalPriceLabel: 'Total',
+            totalPrice: '10',
+            currencyCode: 'IND',
+            countryCode: 'US',
+        },
+    }}
+    onLoadPaymentData={paymentRequest => {
+        console.log('load payment data', paymentRequest);
+    }}
+    onPaymentAuthorized={paymentData => {
+        console.log('Payment Authorised Sucess', paymentData)
+        return { transactionState: 'SUCCESS' }
+      }
+    }
+    />
     )
 }
 
