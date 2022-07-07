a02850ab3e97a60ce43c33acbc4767fcef808605
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-07-07 15:10:08 -0400

    Update donate.js

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -42,6 +42,7 @@ function Donate() {
           onLoadPaymentData={paymentRequest => {
             console.log('load payment data', paymentRequest);
           }}
+          buttonType = "donate"
         />
       </div>  
     )
