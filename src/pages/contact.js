48eb872dd67eeebedbed62f7b011816756dbfbec
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-07-07 15:09:10 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,20 @@ function Contact() {
     return (
         <div>
             <h1 className="text">Contact</h1>
+         <form id="contactform" method="POST" onsubmit={this.handleSubmit.bind(this)}>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+             <label for="Message">Message</label>
+             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
         </div>
+        
     )
 }
 
