403692e5e8d3f78fd2f7151d419c8929083074a8
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2022-07-07 15:24:12 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ function Contact() {
          <form id="contactform" method="POST" onsubmit={this.handleSubmit.bind(this)}>
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div class="form-group">
