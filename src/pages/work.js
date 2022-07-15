e0f5a971918f5ec362167371525b38f1cbc20125
aarugunj27 <aarugunj27@ncs.charter.k12.de.us>
2022-07-15 14:49:20 -0400

    work.js

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,39 +1,64 @@
-import { Card } from 'react-bootstrap';
-import React from "react";
-import './static/css/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
 
 function Work() {
-    return (
-        <div className="content-summary-section">
-        <div classname="container p-10 my-5">
-          <div classname="card-deck no-select">
-            <Card className="card card-hover p-2">
-            <svg width="3em" height="3em" viewBox="0 0 16 16" className="fas fa-book-open text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
+  return (
+    <section class="content-summary-section">
+    <div data-aos="fade-up" data-aos-duration="1300" data-aos-once="true">
+      <div class="container p-10 my-5">
+        <div class="card-deck no-select">
+
+          <div class="card card-hover p-2" onclick="window.location.href='https://medicomm.org/products/Covid-19/testing.html'">
+            <div class="card-body">
+              <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-clipboard-data text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
                 <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
                 <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z" />
-            </svg>
-                <Card.Title className="card-title">Education</Card.Title>
-                <Card.Text className="card-text">
-                    Test text Test Text
-                </Card.Text>
-            </Card>
-            <Card className="card card-hover p-2">
-            <svg width="3em" height="3em" viewBox="0 0 16 16" className="fas fa-book-open text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
+              </svg>
+              <h1 class="card-title font-weight-bold">Covid-19 Testing</h1>
+              <p class="card-text text-muted">This link will allow you to come get tested</p>
+            </div>
+          </div>
+
+          <div class="card card-hover p-2" onclick="window.location.href='https://medicomm.org/products/Covid-19/testing.html'">
+            <div class="card-body">
+              <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-clipboard-data text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
                 <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
                 <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z" />
-            </svg>
-                <Card.Title className="card-title">Education</Card.Title>
-                <Card.Text className="card-text">
-                    Test text Test Text
-                </Card.Text>
-            </Card>
-           </div>
-           </div>
+              </svg>
+              <h1 class="card-title font-weight-bold">Covid-19 Testing</h1>
+              <p class="card-text text-muted">This link will allow you to come get tested</p>
+            </div>
+          </div>
+
+          <div class="card card-hover p-2" onclick="window.location.href='https://medicomm.org/products/Covid-19/testing.html'">
+            <div class="card-body">
+              <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-clipboard-data text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                <path d="M4 11a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1zm6-4a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0V7zM7 9a1 1 0 0 1 2 0v3a1 1 0 1 1-2 0V9z" />
+              </svg>
+              <h1 class="card-title font-weight-bold">Covid-19 Testing</h1>
+              <p class="card-text text-muted">This link will allow you to come get tested</p>
+            </div>
+          </div>
+
+          <div class="card card-hover p-2" onclick="window.location.href='https://medicomm.org/products/Covid-19/kits.html'">
+            <div class="card-body">
+              <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-easel-fill text-danger my-3" fill="#ededed" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.473.337a.5.5 0 0 0-.946 0L6.954 2h2.092L8.473.337zM12.15 11h-1.058l1.435 4.163a.5.5 0 0 0 .946-.326L12.15 11zM8.5 11h-1v2.5a.5.5 0 0 0 1 0V11zm-3.592 0H3.85l-1.323 3.837a.5.5 0 1 0 .946.326L4.908 11z" />
+                <path fill-rule="evenodd" d="M14 3H2v7h12V3zM2 2a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+              </svg>
+              <h1 class="card-title font-weight-bold">Covid-19 Kits</h1>
+              <p class="card-text text-muted">This will allow you to get a home testing kit</p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  </section>
+  );
 }
 
 export default Work;
